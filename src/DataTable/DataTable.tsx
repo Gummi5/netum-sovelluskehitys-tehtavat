e2954f8fff314f0f4c8ca53c32eb8017f1401ebd
Juho Kumara <juho.kumara@gmail.com>
2022-02-01 22:19:28 +0200

    Added sort and edited CSS

--- src/DataTable/DataTable.tsx
+++ src/DataTable/DataTable.tsx
@@ -4,6 +4,7 @@ import TableRow, { IRowData } from './TableRow'
 // Contains table for data and add dialog
 function DataTable() {
   const [allData, setAllData] = useState<IRowData[]>([])
+  const [sorted, setSorted] = useState([false, false, false])
 
   // Deletes a row by id
   const deleteRow = (id : number) : void => {
@@ -12,12 +13,9 @@ function DataTable() {
   }
   // Adds a new row of given data
   const addNew = (newData : IRowData) => {
-      const rowData = allData
-      console.log(newData)
-      console.log(allData)
-      rowData.push(newData)
-      setAllData([...rowData])
-      console.log(allData[0])
+    const rowData = allData
+    rowData.push(newData)
+    setAllData([...rowData])
   }
 
   // Replaces an existing row with given data
@@ -26,16 +24,77 @@ function DataTable() {
       deleteRow(id)
   }
 
+  // Sorts people by column headers
+  const sortTable = (columnHeader : string) => {
+    const newData = allData
+
+    if(columnHeader === "firstName") {
+      if(sorted[0] !== true) {
+        newData.sort((person1, person2) => {
+          if (person1.firstName.toUpperCase() < person2.firstName.toUpperCase()) return -1
+          if (person1.firstName.toUpperCase() > person2.firstName.toUpperCase()) return 1
+          return 0
+        })
+        setSorted([true, false, false])
+      }
+      else {
+        newData.sort((person1, person2) => {
+          if (person1.firstName.toUpperCase() > person2.firstName.toUpperCase()) return -1
+          if (person1.firstName.toUpperCase() < person2.firstName.toUpperCase()) return 1
+          return 0
+        })
+        setSorted([false, false, false])
+      }
+    }
+    else if(columnHeader === "lastName") {
+      if(sorted[1] !== true) {
+        newData.sort((person1, person2) => {
+          if (person1.lastName.toUpperCase() < person2.lastName.toUpperCase()) return -1
+          if (person1.lastName.toUpperCase() > person2.lastName.toUpperCase()) return 1
+          return 0
+        })
+        setSorted([false, true, false])
+      }
+      else {
+        newData.sort((person1, person2) => {
+          if (person1.lastName.toUpperCase() > person2.lastName.toUpperCase()) return -1
+          if (person1.lastName.toUpperCase() < person2.lastName.toUpperCase()) return 1
+          return 0
+        })
+        setSorted([false, false, false])
+      }
+    }
+    else if(columnHeader === "age") {
+      if(sorted[2] !== true) {
+        newData.sort((person1, person2) => {
+          if (person1.age < person2.age) return -1
+          if (person1.age > person2.age) return 1
+          return 0
+        })
+        setSorted([false, false, true])
+      }
+      else {
+        newData.sort((person1, person2) => {
+          if (person1.age > person2.age) return -1
+          if (person1.age < person2.age) return 1
+          return 0
+        })
+        setSorted([false, false, false])
+      }
+    }
+    setAllData([...newData])
+  }
+
   return (
     <div className="contentbody">
       <div className="datatable">
       <table>
         <thead>
           <tr>
-            <th id="darkcell">Etunimi</th>
-            <th id="darkcell">Sukunimi</th>
-            <th id="darkcell">Ikä</th>
-            <th id="darkcell">Muokkaa/poista</th>
+            <th onClick={() => sortTable("firstName")} id="darkcell">Etunimi</th>
+            <th onClick={() => sortTable("lastName")} id="darkcell">Sukunimi</th>
+            <th onClick={() => sortTable("age")} id="darkcell">Ikä</th>
+            <th>Muokkaa/poista</th>
           </tr>
         </thead>
         <tbody>
